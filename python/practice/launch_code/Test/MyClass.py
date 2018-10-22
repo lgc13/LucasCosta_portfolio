fed2d553ade5817843983a335c17c619d0f82b4c
Lucas Costa <lucas.gomes.costa@gmail.com>
2018-10-21 21:19:15 -0500

    adding test file

---
+++ python/practice/launch_code/Test/MyClass.py
@@ -0,0 +1,22 @@
+class User:
+    print('Im in class')
+
+    def __init__(self, username, password):
+        self.username = username
+        self._pass = password
+
+    def setName(self, somename):
+        self.username = somename
+
+    def getName(self):
+        return self.username
+
+
+    def test(self):
+        print('in test')
+
+    a = 1 + 1
+    print a
+
+# a = MyClass()
+# a.test()
---
+++ python/practice/launch_code/Test/main.py
@@ -0,0 +1,13 @@
+from MyClass import User
+imort dbstuff
+
+# create user
+
+def createUser(some_user):
+    some_user.setName('Lucas')
+
+
+b = User('Jeff', 'Lname')
+print b.getName()
+createUser(b)
+# b.test()
