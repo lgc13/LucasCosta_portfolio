f923f577fe3c7c3b5506ec61129fef490c45b897
Lucas C <lucas_costa@unigroup.com>
2018-10-29 13:50:20 -0500

    created symlinks for dotfiles, which hold some aliases, personal themes and settings

---
+++ .dotfiles/create_links.sh
@@ -0,0 +1,5 @@
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
+
