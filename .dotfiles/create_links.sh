07dbfda6829dada8538ba0598659f8f45e4dff5c
Lucas C <lucas_costa@unigroup.com>
2018-11-09 09:12:06 -0600

    made small changes on bash_profile and create_links.sh

--- .dotfiles/create_links.sh
+++ .dotfiles/create_links.sh
@@ -1,5 +1,6 @@
+# Lucas Costa's personal create_links script, to create symlinks for my .dotfiles stuff
+
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
-
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
