d10d84b4aa92fc46e10a9207204db0a0fa5c77fd
Lucas C <lucas_costa@unigroup.com>
2018-11-09 09:37:03 -0600

    added cpwd alias

--- .dotfiles/create_links.sh
+++ .dotfiles/create_links.sh
@@ -1,4 +1,5 @@
 # Lucas Costa's personal create_links script, to create symlinks for my .dotfiles stuff
+# DON'T FORGET, I need to 'chmod 7674 file_name.sh' in order to execute this
 
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
