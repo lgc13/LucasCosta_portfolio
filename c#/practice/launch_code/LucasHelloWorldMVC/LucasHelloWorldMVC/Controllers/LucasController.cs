348577afe2fbc5014162009fe1caf41d4b39ecad
Lucas Costa <lucas.gomes.costa@gmail.com>
2018-11-10 17:01:41 -0600

    playing with lucashelloworld

---
+++ c#/practice/launch_code/LucasHelloWorldMVC/LucasHelloWorldMVC/Controllers/LucasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LucasHelloWorldMVC.Controllers
+{
+    public class LucasController
+    {
+        public LucasController 
+        {
+            // 
+            // GET: /HelloWorld/
+
+            public string Index()
+            {
+                return "This is my default action...";
+            }
+
+            // 
+            // GET: /HelloWorld/Welcome/ 
+
+            public string Welcome()
+            {
+                return "This is the Welcome action method...";
+            }
+        }
+    }
+}
