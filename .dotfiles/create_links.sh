959d932953bc2eb34b4befb503edba3fef73133e
Lucas C <lucas_costa@unigroup.com>
2018-11-12 16:28:05 -0600

    adding atom package list and testing techjobsconsole

--- .dotfiles/create_links.sh
+++ .dotfiles/create_links.sh
@@ -5,3 +5,4 @@ ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/atom-package.list ~/.atom/package.list
