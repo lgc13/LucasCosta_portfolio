739fd6b58080c2247de01eca336b27ce4241d11c
Lucas C <lucas_costa@unigroup.com>
2018-11-13 09:50:26 -0600

    changing atom package and scripts

--- .dotfiles/scripts/atom-get-packages.sh
+++ .dotfiles/scripts/atom-get-packages.sh
@@ -1,4 +1,7 @@
 # This is used to save the Atom packages to a file
 
 echo 'Getting packages from local Atom, and saving them to ~/.atom/package.list'
-apm list --installed --bare > ~/.atom/package.list
+# apm list --installed --bare > ~/.atom/package.list
+
+# This supposably filters the version info
+apm list --installed --bare | grep '^[^@]\+' -o > ~/.atom/package.list
--- .dotfiles/scripts/atom-install-packages.sh
+++ .dotfiles/scripts/atom-install-packages.sh
@@ -1,4 +1,6 @@
 # This install all Atom packages found in the atom-package.list onto local Atom
 
-echo 'Installing atom packages from ~/.atom/package.list'
+echo 'Installing atom packages from ~/.atom/package.list... :) '
 apm install --packages-file ~/.atom/package.list
+
+echo 'DONE! YOU DID IT!'
