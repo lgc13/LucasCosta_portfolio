a2263220b7095aa45399bdffb67735796c02101a
Lucas C <lucas_costa@unigroup.com>
2018-11-13 09:15:42 -0600

    added la alias, made scripts executable, made ln -s verbose

--- .dotfiles/scripts/create_links.sh
+++ .dotfiles/scripts/create_links.sh
@@ -1,8 +1,8 @@
 # Lucas Costa's personal create_links script, to create symlinks for my .dotfiles stuff
 # DON'T FORGET, I need to 'chmod 764 file_name.sh' in order to execute this
 
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
-ln -sh ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/atom-package.list ~/.atom/package.list
+ln -sv ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
+ln -sv ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
+ln -sv ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
+ln -shv ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -sv ~/Desktop/LucasCosta_portfolio/.dotfiles/atom-package.list ~/.atom/package.list
