97ea1591a53b2887dd24fc77d9df11e802146f58
Lucas Costa <lucas.gomes.costa@gmail.com>
2018-11-12 19:06:57 -0600

    fixing oh-my-zsh symlink

--- .dotfiles/create_links.sh
+++ .dotfiles/create_links.sh
@@ -1,8 +1,8 @@
 # Lucas Costa's personal create_links script, to create symlinks for my .dotfiles stuff
-# DON'T FORGET, I need to 'chmod 7674 file_name.sh' in order to execute this
+# DON'T FORGET, I need to 'chmod 764 file_name.sh' in order to execute this
 
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/vimrc ~/.vimrc
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/zshrc ~/.zshrc
-ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -sh ~/Desktop/LucasCosta_portfolio/.dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/Desktop/LucasCosta_portfolio/.dotfiles/atom-package.list ~/.atom/package.list
