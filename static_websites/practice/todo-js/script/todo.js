a90e8fa88a2c256999af3bbf79d0d1feea338bed
Lucas Costa <lucas_costa@unigroup.com>
2019-04-26 17:10:37 -0500

    added functionality so that items that are marked complete show a line through, and also have their field complete changed

--- static_websites/practice/todo-js/script/todo.js
+++ static_websites/practice/todo-js/script/todo.js
@@ -5,17 +5,48 @@ let todos = [
   {id: 4, text: "Write thank-you notes", complete: false},
 ];
 
-const displayTodo = () => {
+const displayInitialTodo = () => {
   const mainTodoList = document.getElementById('main-todo-list');
-  const todoDiv = document.getElementsByClassName('todo')[0];
+  const todoDiv = document.getElementsByClassName('todo')[0]; // make a copy of main todo div
 
-  document.getElementsByClassName('todo')[0].remove();
+  document.getElementsByClassName('todo')[0].remove(); // delete current
 
   todos.forEach((todo) => {
-    newTodo = todoDiv.cloneNode(true);
-    newTodo.getElementsByClassName('todo-text')[0].innerHTML = todo.text;
-    mainTodoList.appendChild(newTodo);
+    newTodoDiv = todoDiv.cloneNode(true);
+    newTodoDiv.getElementsByTagName('span')[0].innerHTML = todo.text;
+    newTodoDiv.getElementsByTagName('span')[0].id = todo.id;
+    styleTodoItem(todo, newTodoDiv);
+    mainTodoList.appendChild(newTodoDiv);
   });
 }
 
-displayTodo();
+const styleTodoItem = (todoItem, divTodo) => {
+
+  if (todoItem.complete) {
+    divTodo.className = 'todo complete';
+  } else {
+    divTodo.className = 'todo';
+  }
+}
+
+
+const checkBoxPress = (input) => {
+  const todoId = Number(input.parentElement.getElementsByClassName('todo-text')[0].id); // get todoId of item clicked on
+
+  const todoItem = todos.find(todo => todo.id === todoId); // find item with that id
+
+  todoItem.complete ? todoItem.complete = false : todoItem.complete = true;
+
+  styleTodoItem(todoItem, input.parentElement);
+
+  todos.filter((todo) => todo.complete == true )
+    .forEach((todo) => {console.log('true:' , todo.text)});
+  console.log('------');
+}
+
+
+displayInitialTodo();
+
+todos.filter((todo) => todo.complete == true )
+  .forEach((todo) => {console.log('true:' , todo.text)});
+console.log('------');
