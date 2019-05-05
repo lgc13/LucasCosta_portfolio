18ca7b76a8102a5de6daec6c1663dafdaa078487
Lucas Costa <lucas.gomes.costa@gmail.com>
2019-05-05 15:50:32 -0500

    working on toin coss

--- react/React-Tutorial/Day1/practice-apps/src/index.js
+++ react/React-Tutorial/Day1/practice-apps/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div>
-      <p> My Practice Apps</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  tossCoin = (coinFace) => {
+    console.log('coinFace', coinFace);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Coin Flip</h2>
+        <h4>Heads or Tails?</h4>
+        <select>
+          <option value="Heads">Heads</option>
+          <option value="Tails">Tails</option>
+        </select>
+        <input
+          type="submit"
+          value="Toss Coin"
+          onClick={() => this.tossCoin('heads')}
+          enabled>
+        </input>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
