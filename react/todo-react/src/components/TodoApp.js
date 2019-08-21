dbcee8a83560c789359b70763aec810561e4c87c
Lucas Costa <lucas_costa@unigroup.com>
2019-08-21 15:58:52 -0500

    eslint changes to TodoApp

--- react/todo-react/src/components/TodoApp.js
+++ react/todo-react/src/components/TodoApp.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../index.css';
 
-import todos from '../initialTodos.js';
-import MainLayout from './MainLayout.js';
+import todos from '../initialTodos';
+import MainLayout from './MainLayout';
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -12,83 +12,89 @@ class TodoApp extends React.Component {
       itemsRemaining: 0,
       newItem: '',
       button: '',
-    }
+    };
   }
+
   componentDidMount() {
     this.getInitialItemsRemaining();
   }
+
   getInitialItemsRemaining = () => {
     let itemsCompleted = 0;
-    this.state.todos.forEach(todo => {
+    const { todos } = this.state;
+    todos.forEach(todo => {
       if (todo.complete) {
         itemsCompleted += 1;
       }
     });
     this.setState({
-      itemsRemaining: this.state.todos.length - itemsCompleted,
-      button: 'hide'
+      itemsRemaining: todos.length - itemsCompleted,
+      button: 'hide',
     });
-  }
-  handleOnClick = (todo) => {
+  };
+
+  handleOnClick = todo => {
     const { itemsRemaining } = this.state;
     const newItemsRemaining = todo.complete ? itemsRemaining + 1 : itemsRemaining - 1;
     this.setState({
       itemsRemaining: newItemsRemaining,
-      todos: this.state.todos.map(td => {
+      todos: todos.map(td => {
         if (td.id === todo.id) {
           return {
             ...td,
-            complete: !td.complete
-          }
+            complete: !td.complete,
+          };
         }
         return td;
-      })
+      }),
     });
-  }
-  handleItemTyping = (event) => {
-    this.setState({ newItem: event.target.value});
-  }
-  handleEnterPress = (event) => {
+  };
+
+  handleItemTyping = event => {
+    this.setState({ newItem: event.target.value });
+  };
+
+  handleEnterPress = event => {
     event.preventDefault();
     const { todos, itemsRemaining, newItem } = this.state;
     if (newItem.trim()) {
       const newTodo = {
         id: todos[todos.length - 1].id + 1,
         text: newItem.trim(),
-        complete: false
+        complete: false,
       };
       this.setState({
-        todos: [
-          ...todos,
-          newTodo,
-        ],
+        todos: [...todos, newTodo],
         itemsRemaining: itemsRemaining + 1,
         newItem: '',
       });
     }
-  }
-  handleButtonPress = (button) => {
+  };
+
+  handleButtonPress = button => {
     this.setState({
       button: button === 'hide' ? 'show' : 'hide',
       styles: button === 'hide' ? 'hidden' : '',
     });
-  }
+  };
+
   render() {
+    const { todos, styles, newItem, itemsRemaining, button } = this.state;
     return (
       <MainLayout
         title="Things to do"
-        todos={this.state.todos}
+        todos={todos}
         onClick={this.handleOnClick}
-        styles={this.state.styles}
-        newItem={this.state.newItem}
+        styles={styles}
+        newItem={newItem}
         onChange={this.handleItemTyping}
         onSubmit={this.handleEnterPress}
-        itemsRemaining={this.state.itemsRemaining}
-        button={this.state.button}
+        itemsRemaining={itemsRemaining}
+        button={button}
         onButtonPress={this.handleButtonPress}
       />
-    )
+    );
   }
 }
 
-export default TodoApp
+export default TodoApp;
