bdff889a6f15c94b3e82c847f7762de40523ded4
Lucas Costa <lucas.gomes.costa@gmail.com>
2019-08-25 14:56:22 -0500

    added new version of heads tails, rock paper scissors games

---
+++ react/heads-or-scissors-games/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HeadsOrScissors from './HeadsOrScissors';
+
+ReactDOM.render(
+  <HeadsOrScissors />,
+  document.getElementById('root')
+);
