60ecf7833beae6bfa74df6b2b94cede211a69db6
Lucas Costa <lucas_costa@unigroup.com>
2020-02-04 09:40:17 -0600

    adding script to check all repos to master, and added LC_PORTFOLIO_DIR as an env var

---
+++ .dotfiles/scripts/git-checkout-master-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+echo "Checking out the following repos to master:"
+
+CURRENT_DIR=`pwd`
+
+for repo in "${CURRENT_DIR}/"*; do
+    if [[ -d "$repo/.git" ]] then
+        cd $repo
+        BRANCH=$(git rev-parse --abbrev-ref HEAD)
+        if [[ $BRANCH != "master" ]] then
+            echo "repo: $repo"
+            git checkout master
+        fi
+    fi
+done
