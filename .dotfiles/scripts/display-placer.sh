389437d9fb855dddc755f8a9418277a59307f4af
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-03-12 16:36:50 -0500

    added displayPlacer script

---
+++ .dotfiles/scripts/display-placer.sh
@@ -0,0 +1,4 @@
+# DON'T FORGET, I need to 'chmod 764 file_name.sh' in order to execute this
+
+# displayPlacer github: https://github.com/jakehilborn/displayplacer
+displayplacer "id:D697838B-4FBB-7D87-7202-1BFF6A49AF7E res:2048x1152 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:3FCB7096-B762-AC7D-4FD3-E8D9DB460CFB res:1680x1050 color_depth:4 scaling:on origin:(-1680,102) degree:0" "id:39D5923D-F764-68DB-E36B-6ADEFB734EAA res:2048x1152 hz:60 color_depth:8 scaling:off origin:(2048,0) degree:0"
