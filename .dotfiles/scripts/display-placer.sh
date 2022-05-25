d3a6fea8c7242cec275fa46440a5580dbe8fe23d
Lucas Costa <lucas.gomes.costa@gmail.com>
2022-05-24 21:43:24 -0400

    new display placer

--- .dotfiles/scripts/display-placer.sh
+++ .dotfiles/scripts/display-placer.sh
@@ -1,4 +1,4 @@
 # DON'T FORGET, I need to 'chmod 764 file_name.sh' in order to execute this
 
 # displayPlacer github: https://github.com/jakehilborn/displayplacer
-displayplacer "id:D697838B-4FBB-7D87-7202-1BFF6A49AF7E res:2048x1152 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0" "id:3FCB7096-B762-AC7D-4FD3-E8D9DB460CFB res:1680x1050 color_depth:4 scaling:on origin:(-1680,102) degree:0" "id:39D5923D-F764-68DB-E36B-6ADEFB734EAA res:2048x1152 hz:60 color_depth:8 scaling:off origin:(2048,0) degree:0"
+displayplacer "id:6321C91B-2286-E77D-72D6-9A3CC0A96BDE res:1792x1120 hz:59 color_depth:8 scaling:on origin:(0,0) degree:0" "id:C56CF534-D2D1-F18D-486D-FA23EF96F8EB res:2560x1440 hz:59 color_depth:8 scaling:off origin:(4352,-608) degree:0" "id:B0A36E13-821B-5910-61F3-53D42ADDBD02 res:2560x1440 hz:59 color_depth:8 scaling:off origin:(1792,-608) degree:0"
